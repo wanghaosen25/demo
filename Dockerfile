4425f1e1355daa31fe9c0157a0dc7e234c5e0040
xkh01 <xkh0917@gmail.com>
2021-10-16 19:49:08 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM teddysun/xray
 ADD run.sh /
 RUN chmod +x /run.sh
-CMD run.sh
+CMD /run.sh
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-cat<<EOF>/etc/cfg.json
+cat<<EOF>/etc/config.json
 {
 	"inbounds":[{
 		"port":$PORT,
@@ -18,4 +18,4 @@ cat<<EOF>/etc/cfg.json
 	}]
 }
 EOF
-xray -c /etc/cfg.json
+xray -c /etc/config.json
