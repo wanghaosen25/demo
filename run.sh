a5744478d06ac90c9522ac2854e426b56e72efa4
xkh01 <xkh0917@gmail.com>
2021-10-17 19:42:33 +0800

    update:

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 cat<<EOF>/cfg.json
-{"inbounds":[{"port":$PORT,"protocol":"vless","settings":{"decryption":"none","clients":[{"id":"$id"}]},"streamSettings":{"network":"ws"}}],"outbounds":[{"protocol":"freedom"}]}
+{"inbounds":[{"port":443,"protocol":"vless","settings":{"decryption":"none","clients":[{"id":"$id"}]},"streamSettings":{"network":"ws"}}],"outbounds":[{"protocol":"freedom"}]}
 EOF
 xray -c /cfg.json
