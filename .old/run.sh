4fcf8b0a226056e7701d7acdfb211827a111e407
xkh01 <xkh0917@gmail.com>
2021-10-24 20:18:18 +0800

    upd

---
+++ .old/run.sh
@@ -0,0 +1,21 @@
+cat<<EOF>/etc/config.json
+{
+	"inbounds":[{
+		"port":$PORT,
+		"protocol":"vless",
+		"settings":{
+			"decryption":"none",
+			"clients":[{
+				"id":"$id"
+			}]
+		},
+		"streamSettings":{
+			"network":"ws"
+		}
+	}],
+	"outbounds":[{
+		"protocol":"freedom"
+	}]
+}
+EOF
+/usr/bin/xray -c /etc/config.json
