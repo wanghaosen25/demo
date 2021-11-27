4873b9011a686a8abfd0606468a1d28605731858
xkh01 <xkh0917@gmail.com>
2021-11-27 14:40:35 +0800

    upd

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
+wget github.com/xtls/xray-core/releases/latest/download/xray-linux-64.zip -O /dist.zip 
+unzip /dist.zip -d /xray
 cat<<EOF>/cfg.json
 {"inbounds":[{"port":$PORT,"protocol":"vless","settings":{"decryption":"none","clients":[{"id":"$id"}]},"streamSettings":{"network":"ws"}}],"outbounds":[{"protocol":"freedom"}]}
 EOF
-xray -c /cfg.json
+/xray/xray -c /cfg.json
